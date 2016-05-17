8814ca2f53001bbf1ab3808065d7539d1c9e021d
Mogens Heller Grabe <mookid8000@gmail.com>
2016-05-17 22:48:57 +0200

    print basedir

---
+++ Owino.Tests/Extensions/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Owino.Tests.Extensions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void PrintAppDomainPath()
+        {
+            Console.WriteLine($"basedi: {AppDomain.CurrentDomain.BaseDirectory}");
+        }
+    }
+}
